ba33ad55d44cc15c015f2e07d3d2d9861bbf058f
Nanduyar <57687840+Nanduyar@users.noreply.github.com>
2019-11-30 19:47:20 -0300

    Add files via upload

---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,133 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
+
+/* LOGICA PIN LOGIN */
+
+class PinLogin {
+    constructor ({el, loginEndpoint, redirectTo, maxNumbers = Infinity}) {
+        this.el = {
+            main: el,
+            numPad: el.querySelector(".pin-login__numpad"),
+            textDisplay: el.querySelector(".pin-login__text")
+        };
+
+        this.loginEndpoint = loginEndpoint;
+        this.redirectTo = redirectTo;
+        this.maxNumbers = maxNumbers;
+        this.value = "";
+
+        this._generatePad();
+    }
+
+    _generatePad() {
+        const padLayout = [
+            "1", "2", "3",
+            "4", "5", "6",
+            "7", "8", "9",
+            "backspace", "0", "done"
+        ];
+
+        padLayout.forEach(key => {
+            const insertBreak = key.search(/[369]/) !== -1;
+            const keyEl = document.createElement("div");
+
+            keyEl.classList.add("pin-login__key");
+            keyEl.classList.toggle("material-icons", isNaN(key));
+            keyEl.textContent = key;
+            keyEl.addEventListener("click", () => { this._handleKeyPress(key) });
+            this.el.numPad.appendChild(keyEl);
+
+            if (insertBreak) {
+                this.el.numPad.appendChild(document.createElement("br"));
+            }
+        });
+    }
+
+    _handleKeyPress(key) {
+        switch (key) {
+            case "backspace":
+                this.value = this.value.substring(0, this.value.length - 1);
+                break;
+            case "done":
+                this._attemptLogin();
+                break;
+            default:
+                if (this.value.length < this.maxNumbers && !isNaN(key)) {
+                    this.value += key;
+                }
+                break;
+        }
+
+        this._updateValueText();
+    }
+
+    _updateValueText() {
+        this.el.textDisplay.value = "_".repeat(this.value.length);
+        this.el.textDisplay.classList.remove("pin-login__text--error");
+    }
+
+    _attemptLogin() {
+        if (this.value.length > 0) {
+            fetch(this.loginEndpoint, {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: `pincode=${this.value}`
+            }).then(response => {
+                if (response.status === 200) {
+                    window.location.href = this.redirectTo;
+                } else {
+                    this.el.textDisplay.classList.add("pin-login__text--error");
+                }
+            })
+        }
+    }
+}
+
+new PinLogin({
+    el: document.getElementById("mainPinLogin"),
+    loginEndpoint: "indexLogin.html",
+    redirectTo: "index.html"
+});
